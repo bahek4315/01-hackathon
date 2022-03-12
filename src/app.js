d8d5e766fbc8ec964e340feb6c2829e6e4ce8324
bahek4315 <bahek4315@yandex.ru>
2022-03-12 18:28:16 +0300

    changed contextmenu order

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import {ContextMenu} from './menu';
 import {FigureModule} from './modules/figure.module'
 
 const menu = new ContextMenu('.menu');
-menu.add(new CustomMessage('message', 'Кастомное сообщение'));
 menu.add(new Greet('greet', 'Приветствие'));
+menu.add(new CustomMessage('message', 'Кастомное сообщение'));
 menu.add(new FigureModule('figure', 'Создать рандомную фигуру'));
 menu.open();
