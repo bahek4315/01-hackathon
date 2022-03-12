baed16c61e586ba3f1e2f2774344c4e5f81d6806
bahek4315 <bahek4315@yandex.ru>
2022-03-13 01:44:38 +0300

    changed sounds

--- src/modules/sound.module.js
+++ src/modules/sound.module.js
@@ -2,9 +2,8 @@ import {Module} from '../core/module';
 import { random } from '../utils';
 
 export class Sound extends Module { 
-  trigger() {  
-    const arrSound = ['/sound/1.mp3', '/sound/2.mp3', '/sound/3.mp3'];
-    const audio = new Audio(arrSound[random(0, arrSound.length - 1)]);
-    audio.play(); 
+  trigger() {
+    const audio = new Audio(`/sound/${random(1,10)}.mp3`);
+    audio.play();
   }
 }
