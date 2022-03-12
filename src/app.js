9e5941be50315adf5f33cbc4f9e6400e51531c18
Ivan <70642528+bahek4315@users.noreply.github.com>
2022-03-12 15:46:56 +0300

    Update app.js
    Redid app.js, was extra imports

--- src/app.js
+++ src/app.js
@@ -3,15 +3,6 @@ import {Greet} from './modules/greet.module';
 import {ContextMenu} from './menu';
 
 const menu = new ContextMenu('.menu');
-
-menu.add(new Greet('greet', 'Приветствие'));
-menu.open();
-
-
-import { CustomMessage } from './modules/message.module';
-
-import {ContextMenu} from './menu';
-
-const menu = new ContextMenu('.menu');
 menu.add(new CustomMessage('message', 'Кастомное сообщение'));
+menu.add(new Greet('greet', 'Приветствие'));
 menu.open();
