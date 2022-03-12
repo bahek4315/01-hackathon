9488f6c48bd4d12247b1cf00970184628937df03
mitrofanushkakys <93116011+mitrofanushkakys@users.noreply.github.com>
2022-03-13 00:59:50 +0400

    Добавил модуль sound

--- src/app.js
+++ src/app.js
@@ -1,2 +1,9 @@
-import './styles.css'
+import './styles.css';
+import { Sound } from './modules/sound.module';
+import { ContextMenu } from './menu';
 
+const menu = new ContextMenu('.menu');
+
+// menu.add(new Greet('greet'), 'dsadsa');
+menu.add(new Sound('audio', 'Воспроизвести аудио'));
+menu.open();
