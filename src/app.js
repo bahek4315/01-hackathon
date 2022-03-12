fc4c9e11a2a47208295c03216e6ebb82124104de
Vladimirova Ekaterina <echapyrkina@mail.ru>
2022-03-12 16:36:55 +0300

    add module customMessage

--- src/app.js
+++ src/app.js
@@ -1,2 +1,9 @@
 import './styles.css'
 
+import { CustomMessage } from './modules/message.module';
+
+import {ContextMenu} from './menu';
+
+const menu = new ContextMenu('.menu');
+menu.add(new CustomMessage('message', 'Кастомное сообщение'));
+menu.open();
