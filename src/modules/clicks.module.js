532190735886b92e555406e1cf3c8759c5ba25ad
bahek4315 <bahek4315@yandex.ru>
2022-03-13 17:18:48 +0300

    added close button

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -5,6 +5,7 @@ export class ClicksModule extends Module {
         if (!document.querySelector('.click-window')) {
             const clickWindow = document.createElement('div');
             clickWindow.classList.add('click-window');
+            clickWindow.style.position = 'relative';
             clickWindow.style.width = '300px';
             clickWindow.style.height = '100px';
             clickWindow.style.margin = '0 auto';
@@ -22,7 +23,7 @@ export class ClicksModule extends Module {
             clickMessage.style.textAlign = 'center';
             clickMessage.textContent = 'Кликай что есть мочи!';
 
-            let time = 10;
+            let time = 5;
 
             const countMessage = document.createElement('span');
             countMessage.textContent = `Осталось ${time} секунд(ы)`;
@@ -60,9 +61,42 @@ export class ClicksModule extends Module {
                 resultMessage.textContent = `${counter - doubleClicks * 2} простых и ${doubleClicks} двойных кликов!`;
                 resultMessage.style.fontSize = '16px';
                 clickWindow.append(resultMessage);
-            }, time * 1000);
 
-            setTimeout(() => clickWindow.remove(), time * 1000 + 4000);
+                const closeButton = document.createElement('button');
+                
+                const closeResults = () => {
+                    clickWindow.remove();
+                    closeButton.removeEventListener('click', closeResults);
+                }
+
+                closeButton.addEventListener('click', closeResults);
+                closeButton.style.position = 'absolute';
+                closeButton.style.width = '25px';
+                closeButton.style.height = '25px';
+                closeButton.style.background = 'none';
+                closeButton.style.border = 'none';
+                closeButton.style.right = '5px';
+                closeButton.style.top = '5px';
+                closeButton.style.cursor = 'pointer';
+
+                const cross1 = document.createElement('div');
+                const cross2 = document.createElement('div');
+                cross1.style.position = 'absolute';
+                cross1.style.width = '25px';
+                cross1.style.height = '2px';
+                cross1.style.backgroundColor = '#6bbdf3'
+                cross1.style.transform = 'rotate(45deg)';
+                cross2.style.position = 'absolute';
+                cross2.style.width = '25px';
+                cross2.style.height = '2px';
+                cross2.style.backgroundColor = '#6bbdf3'
+                cross2.style.transform = 'rotate(-45deg)';
+                
+                closeButton.append(cross1);
+                closeButton.append(cross2);   
+                clickWindow.append(closeButton);
+
+            }, time * 1000);
         }
     }
 }
