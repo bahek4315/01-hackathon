fe9d215700eec9f88c93ea81b9a87d86e7f18261
Vova <kot@gmail.com>
2022-03-13 08:03:39 +0300

    fixed bugs

--- src/menu.js
+++ src/menu.js
@@ -3,11 +3,11 @@ import {Menu} from './core/menu'
 export class ContextMenu extends Menu {
    open() {
       document.body.style.overflow = 'hidden';
-      document.addEventListener('contexmenu', (event) => {
+      document.addEventListener('contextmenu', (event) => {
          event.preventDefault();
          this.el.style.position = 'absolute';
-         this.el.style.top = ${ event.y } px;
-         this.el.style.left = ${ event.x } px;
+         this.el.style.top = `${ event.y } px`;
+         this.el.style.left = `${ event.x } px`;
          this.el.style.display = 'block';
       })
    }
@@ -17,8 +17,8 @@ export class ContextMenu extends Menu {
    }
 
    add(module) {
-      this.el.insertAdjacentHTML('beforeed', module.toHTML());
-      const newModule = this.el.querySelector([data - type='${module.type}']);
+      this.el.insertAdjacentHTML('beforeend', module.toHTML());
+      const newModule = this.el.querySelector([data-type=`${module.type}`]);
       newModule.addEventListener('click', module.trigger);
    }
 }
