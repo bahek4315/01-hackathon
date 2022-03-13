ddc6046391d5a892172db1c37c944faa9f2dbad7
Vladimirova Ekaterina <echapyrkina@mail.ru>
2022-03-13 15:25:14 +0300

    add cards module

--- src/app.js
+++ src/app.js
@@ -2,8 +2,11 @@ import './styles.css';
 import {Greet} from './modules/greet.module';
 import {CustomMessage} from './modules/message.module';
 import {ContextMenu} from './menu';
+import { Cards } from './modules/cards.module';
+
 
 const menu = new ContextMenu('.menu');
 menu.add(new CustomMessage('message', 'Кастомное сообщение'));
 menu.add(new Greet('greet', 'Приветствие'));
+menu.add(new Cards('cards', 'Выбери кота'));
 menu.open();
