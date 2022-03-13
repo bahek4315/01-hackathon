52ac393723508fed3978b43bcee9d1e39c532dcf
bahek4315 <bahek4315@yandex.ru>
2022-03-13 14:23:48 +0300

    did clicker without close button

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ import {Greet} from './modules/greet.module';
 import { Sound } from './modules/sound.module';
 import {CustomMessage} from './modules/message.module';
 import {ContextMenu} from './menu';
-import {FigureModule} from './modules/figure.module'
+import {FigureModule} from './modules/figure.module';
+import {ClicksModule} from './modules/clicks.module';
 
 const menu = new ContextMenu('.menu');
 menu.add(new Greet('greet', 'Приветствие'));
 menu.add(new CustomMessage('message', 'Кастомное сообщение'));
 menu.add(new FigureModule('figure', 'Создать рандомную фигуру'));
 menu.add(new Sound('audio', 'Воспроизвести аудио'));
+menu.add(new ClicksModule('clicks', 'Подсчет кликов'));
 menu.open();
--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,7 +1,70 @@
 import {Module} from '../core/module';
 
 export class ClicksModule extends Module {
- 
+    trigger() {
+        if (!document.querySelector('.click-window')) {
+            const clickWindow = document.createElement('div');
+            clickWindow.classList.add('click-window');
+            clickWindow.style.width = '300px';
+            clickWindow.style.height = '100px';
+            clickWindow.style.margin = '0 auto';
+            clickWindow.style.backgroundColor = '#313131';
+            clickWindow.style.color = '#6bbdf3';
+            clickWindow.style.borderRadius = '5px';
+            clickWindow.style.display = 'flex';
+            clickWindow.style.flexDirection = 'column';
+            clickWindow.style.justifyContent = 'center';
+            clickWindow.style.alignItems = 'center';
+
+            const clickMessage = document.createElement('span');
+            clickMessage.style.fontSize = '22px';
+            clickMessage.style.marginBottom = '10px';
+            clickMessage.style.textAlign = 'center';
+            clickMessage.textContent = 'Кликай что есть мочи!';
+
+            let time = 10;
+
+            const countMessage = document.createElement('span');
+            countMessage.textContent = `Осталось ${time} секунд(ы)`;
+
+            let counter = -1;
+            let doubleClicks = 0;
+
+            clickWindow.append(clickMessage);
+            clickWindow.append(countMessage);
+            document.body.append(clickWindow);
+
+            const interval = setInterval(() => {
+                time--;
+                countMessage.textContent = `Осталось ${time} секунд(ы)`;
+            }, 1000);
+
+            const clickCounting = () => {
+                counter++;
+            };
+
+            const doubleclickCounting = () => {
+                doubleClicks++;
+            };
+
+            document.addEventListener('click', clickCounting);
+            document.addEventListener('dblclick', doubleclickCounting);
+
+            setTimeout(() => {
+                clearInterval(interval);
+                countMessage.remove();
+                document.removeEventListener('click', clickCounting);
+                document.removeEventListener('dblclick', doubleclickCounting);
+                clickMessage.textContent = 'Ваш результат:';
+                const resultMessage = document.createElement('span');
+                resultMessage.textContent = `${counter - doubleClicks * 2} простых и ${doubleClicks} двойных кликов!`;
+                resultMessage.style.fontSize = '16px';
+                clickWindow.append(resultMessage);
+            }, time * 1000);
+
+            setTimeout(() => clickWindow.remove(), time * 1000 + 4000);
+        }
+    }
 }
 
 
