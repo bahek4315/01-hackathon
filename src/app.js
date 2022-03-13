0c99b99def87a8a037d66e5126450c810aaa3337
Vova <kot@gmail.com>
2022-03-13 07:40:34 +0300

    add feature/background-module in develop

--- src/app.js
+++ src/app.js
@@ -1,2 +1,7 @@
-import './styles.css'
+import './styles.css';
+import { BackgroundModule } from './modules/background.module';
+import { ContextMenu } from './menu';
 
+const menu = new ContextMenu('.menu');
+menu.add(new BackgroundModule());
+menu.open();
--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,5 +1,16 @@
-import {Module} from '../core/module'
+import { Module } from '../core/module'
 
 export class BackgroundModule extends Module {
+   trigger() {
+      const button = document.createElement('button');
+      button.classList.add('button');
+      button.textContent = 'Изменить цвет фона';
+      document.body.append(button);
 
+      button.addEventListener('click', () => {
+         document.body.style.backgroundColor = rgb(random(0, 255), random(0, 255), random(0, 255));
+         document.body.style.transition = 'background, .4s';
+      });
+      setTimeout(() => button.remove, 3000);
+}
 }
