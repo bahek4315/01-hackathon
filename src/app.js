987a67af37e4e52004e3005a8651dd0e46645d08
Vova <kot@gmail.com>
2022-03-13 13:45:50 +0300

    fixed a bug in app.js

--- src/app.js
+++ src/app.js
@@ -3,5 +3,5 @@ import { BackgroundModule } from './modules/background.module';
 import { ContextMenu } from './menu';
 
 const menu = new ContextMenu('.menu');
-menu.add(new BackgroundModule());
+menu.add(new BackgroundModule('backround', 'Сменить фон'));
 menu.open();
