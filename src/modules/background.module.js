ee9192acb2e26711b92d7bac05e3e9593d306a4b
bahek4315 <bahek4315@yandex.ru>
2022-03-13 14:46:03 +0300

    redid function

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,16 +1,9 @@
-import { Module } from '../core/module'
+import { Module } from '../core/module';
+import {random} from '../utils';
 
 export class BackgroundModule extends Module {
    trigger() {
-      const button = document.createElement('button');
-      button.classList.add('button');
-      button.textContent = 'Изменить цвет фона';
-      document.body.append(button);
-
-      button.addEventListener('click', () => {
-         document.body.style.backgroundColor = rgb(random(0, 255), random(0, 255), random(0, 255));
-         document.body.style.transition = 'background, .4s';
-      });
-      setTimeout(() => button.remove, 3000);
-}
+      document.body.style.backgroundColor = `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)}`;
+      document.body.style.transition = 'background 0.4s linear';
+   }
 }
