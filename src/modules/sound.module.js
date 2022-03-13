67635a3e054cfccfda60f88732bfeeac8c12008d
bahek4315 <bahek4315@yandex.ru>
2022-03-13 17:57:11 +0300

    changed audio access

--- src/modules/sound.module.js
+++ src/modules/sound.module.js
@@ -1,9 +1,9 @@
 import {Module} from '../core/module';
 import { random } from '../utils';
-
+const AUDIOS_URL = `http://ch68282.tmweb.ru/`;
 export class Sound extends Module { 
   trigger() {
-    const audio = new Audio(`/sound/${random(1,10)}.mp3`);
+    const audio = new Audio(`${AUDIOS_URL}/${random(1, 10)}.mp3`);
     audio.volume = 0.2;
     audio.play();
   }
