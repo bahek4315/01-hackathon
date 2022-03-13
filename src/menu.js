9469050215daf6500d2e445e59a5882554b9409f
Vova <kot@gmail.com>
2022-03-13 11:22:40 +0300

    fixed a bug in menu.js

--- src/menu.js
+++ src/menu.js
@@ -1,13 +1,13 @@
-import {Menu} from './core/menu'
+import { Menu } from './core/menu';
 
 export class ContextMenu extends Menu {
    open() {
-      document.body.style.overflow = 'hidden';
+      document.body.style.overflowX = 'hidden';
       document.addEventListener('contextmenu', (event) => {
          event.preventDefault();
          this.el.style.position = 'absolute';
-         this.el.style.top = `${ event.y } px`;
-         this.el.style.left = `${ event.x } px`;
+         this.el.style.top = `${event.y}px`;
+         this.el.style.left = `${event.x}px`;
          this.el.style.display = 'block';
       })
    }
@@ -18,7 +18,7 @@ export class ContextMenu extends Menu {
 
    add(module) {
       this.el.insertAdjacentHTML('beforeend', module.toHTML());
-      const newModule = this.el.querySelector([data-type=`${module.type}`]);
+      const newModule = this.el.querySelector(`[data-type='${module.type}']`);
       newModule.addEventListener('click', module.trigger);
    }
 }
